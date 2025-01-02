27f5045d2db23a5bc354ee4681b768c8a508bfac
Caio Vidinha <caiovidinha@id.uff.br>
2025-01-02 16:20:53 -0300

    change

--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
-const app = require("./app");
-const dotenv = require("dotenv").config();
-const http = require("http");
+const fs = require("fs");
 const https = require("https");
+const http = require("http");
+const path = require("path");
+const app = require("./app"); // Sua instância do Express
 
-const runServer = (port, server) => {
-  server.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+// Carregue os arquivos do certificado da Let's Encrypt
+const sslOptions = {
+  key: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "privkey.pem")),
+  cert: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "cert.pem")),
+  ca: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "chain.pem")),
 };
 
-const regularServer = http.createServer(app);
-if (process.env.NODE_ENV === "production") {
-  // TODO: CONF SSL
-  // TODO: RODAR NA PORTA 80 E 443
-} else {
-  const serverPort = process.env.PORT ? parseInt(process.env.PORT) : 9000;
-  runServer(serverPort, regularServer);
-}
+// Servidor HTTPS na porta 443
+const httpsServer = https.createServer(sslOptions, app);
+httpsServer.listen(443, () => {
+  console.log("Servidor HTTPS rodando na porta 443");
+});
