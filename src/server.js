9cee550d6310c56ecbf7ba503c143367475d8b41
caiovidinha <caiovidinha@id.uff.br>
2025-06-13 18:38:17 -0300

    server ok

--- src/server.js
+++ src/server.js
@@ -5,20 +5,20 @@ const path = require("path");
 const app = require("./app"); // Sua instância do Express
 
 
-// const sslOptions = {
-//   key: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "privkey.pem")),
-//   cert: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "cert.pem")),
-//   ca: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "chain.pem")),
-// };
+const sslOptions = {
+  key: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "privkey.pem")),
+  cert: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "cert.pem")),
+  ca: fs.readFileSync(path.join("/etc", "letsencrypt", "live", "appgw.predialnet.com.br", "chain.pem")),
+};
 
 
-// const httpsServer = https.createServer(sslOptions, app);
-// httpsServer.listen(443, () => {
-//   console.log("Servidor HTTPS rodando na porta 443");
-// });
+const httpsServer = https.createServer(sslOptions, app);
+httpsServer.listen(443, () => {
+  console.log("Servidor HTTPS rodando na porta 443");
+});
 
 // Servidor HTTP
-const httpsServer = http.createServer(app);
+const httpServer = http.createServer(app);
 httpsServer.listen(5000, () => {
   console.log("Servidor HTTPS rodando na porta 5000");
 });
