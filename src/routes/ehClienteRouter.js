12aa12b38554e86ce3ce1390bd7631632dcf76cc
caiovidinha <caiomdavidinha@gmail.com>
2026-06-05 17:15:24 -0300

    fix

--- src/routes/ehClienteRouter.js
+++ src/routes/ehClienteRouter.js
@@ -1,22 +1,12 @@
 const express = require("express");
 const { ehCliente } = require("../controllers/ehClienteController");
 
+require("dotenv").config();
+
 const ehClienteRouter = express.Router();
 
-const ACTIVATION_SITE_ORIGIN = "https://www.predialnet.com.br";
-
 function ehClienteCors(req, res, next) {
-  const requestOrigin = req.headers.origin;
-
-  if (requestOrigin && requestOrigin !== ACTIVATION_SITE_ORIGIN) {
-    return res.status(403).json({ message: "Origem não permitida." });
-  }
-
-  if (requestOrigin) {
-    res.setHeader("Access-Control-Allow-Origin", ACTIVATION_SITE_ORIGIN);
-    res.setHeader("Vary", "Origin");
-  }
-
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
